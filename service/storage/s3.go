9f12e5ff63142708a78119a12eda6b8188ab3b9e
AH-dark <ahdark@outlook.com>
2022-09-17 21:10:37 +0800

    Fix: path need convert

--- service/storage/s3.go
+++ service/storage/s3.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"github.com/AH-dark/bing-wallpaper/model"
+	"github.com/AH-dark/bing-wallpaper/pkg/util"
 	"github.com/AH-dark/logger"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -16,6 +17,7 @@ import (
 type S3Impl struct {
 	client   *s3.S3
 	uploader *s3manager.Uploader
+	bucket   string
 }
 
 func NewS3() *S3Impl {
@@ -48,20 +50,23 @@ func NewS3() *S3Impl {
 	return &S3Impl{
 		client:   client,
 		uploader: uploader,
+		bucket:   settings["storage_s3_bucket"],
 	}
 }
 
 func (s *S3Impl) Upload(name string, file io.Reader) (*url.URL, error) {
-	base, err := model.GetSettingVal("storage_s3_path")
+	base, err := model.GetSettingVal("storage_s3_base_path")
 	if err != nil {
 		return nil, err
 	}
 
 	key := filepath.Join(base, name)
+	key = util.FormSlash(key)
 
 	_, err = s.uploader.Upload(&s3manager.UploadInput{
-		Key:  &key,
-		Body: file,
+		Key:    &key,
+		Body:   file,
+		Bucket: &s.bucket,
 	})
 	if err != nil {
 		return nil, err
